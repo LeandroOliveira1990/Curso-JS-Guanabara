f7cd737e34d426c536b5ccfaed083a9ecacbc77d
Leandro de Oliveira Silva <leandrooliveirask8@hotmail.com>
2022-09-19 10:18:18 -0300

    Condições parte 2

---
+++ Condições parte 2/condicoes3.js
@@ -0,0 +1,15 @@
+var agora = new Date();
+var hora = agora.getHours();
+
+console.log(`Agora são exatamente ${hora} horas`);
+if (hora >= 6 ) {
+    console.log('Bom Dia');
+} else if(hora <= 18 && hora >= 12 ) {
+    console.log('Boa Tarde');
+}else if(hora > 0 && hora < 6){
+    console.log('Boa Madrugada');
+} 
+
+else{
+    console.log('Boa Noite');
+}
